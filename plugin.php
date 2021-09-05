6d141a3613803d79998c3d992d349fb5025a6ecc
Awilum <sergey.romanenko@flextype.org>
2021-09-05 22:13:02 +0300

    feat(plugin): update plugin

--- plugin.php
+++ plugin.php
@@ -7,11 +7,11 @@ namespace Flextype\Plugin\Redirect;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-$redirect = flextype('registry')->get('plugins.redirect.settings.redirect');
+$redirect = registry()->get('plugins.redirect.settings.redirect');
 
 if (isset($redirect) and count($redirect) > 0) {
     foreach ($redirect as $item) {
-        flextype()->get($item['from'], function (Request $request, Response $response, array $args) use ($item) {
+        app()->get($item['from'], function (Request $request, Response $response, array $args) use ($item) {
             return $response->withRedirect($item['to'], isset($item['status']) ? $item['status'] : 301);
         });
     }
